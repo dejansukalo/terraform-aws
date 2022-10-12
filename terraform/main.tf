d9bc3618cc192ecd224ba2fe5beee8f695e9189a
dejansukalo <dejan.sukalo@gmail.com>
2022-10-12 09:50:50 +0200

    server-ssh-key commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "myapp-server" {
   vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone = var.avail_zone
   associate_public_ip_address = true
-  key_name = "EC2Tutorial2"
+  key_name = "server-ssh-key"
 
   user_data = file("entry-script.sh")
 
